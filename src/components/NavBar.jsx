4ae6c8cb2820af2c4946fc2fac88cd6a37d6ed2a
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-05-23 13:43:38 +0600

    components - add burger menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -24,6 +24,8 @@ export const NavBar = ({ setSelectedPage, selectedPage }) => {
     <nav className="z-40 w-full top-0 fixed py-6">
       <div className=" flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">KUNDUZ</h4>
+
+        {/* Desktop */}
         {isAboveSmallScreens ? (
           <div className="flex justify-between gap-16 font-opensans text-sm  font-semibold">
             <Link
@@ -48,9 +50,74 @@ export const NavBar = ({ setSelectedPage, selectedPage }) => {
             />
           </div>
         ) : (
-          <button>
-            <img alt="menu"/>
+          <button
+            className="rounded-full bg-red p-2 "
+            onClick={() => setIsMenuToggled(!isMenuToggled)}
+          >
+            <svg
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M3 8V6H21V8H3ZM3 13H21V11H3V13ZM3 18H21V16H3V18Z"
+                fill="white"
+              />
+            </svg>
           </button>
+        )}
+
+        {/*Mobile menu */}
+
+        {!isAboveSmallScreens && isMenuToggled && (
+          <div className="h-full w-[300px] bottom-0 right-0 fixed bg-blue ">
+            <div className="flex justify-end p-12 ">
+              <button onClick={() => setIsMenuToggled(!isMenuToggled)}>
+                <svg
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z"
+                    fill="white"
+                    fillOpacity="0.54"
+                  />
+                </svg>
+              </button>
+            </div>
+
+            {/* MENU ITEMS */}
+
+            <div className="flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            </div>
+          </div>
         )}
       </div>
     </nav>
