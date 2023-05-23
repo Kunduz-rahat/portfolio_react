fd9faf57f4138485c3f2f0440cddfd3bd88ea54c
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-05-23 13:14:06 +0600

    add assets

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,21 @@
 import React, { useState } from "react";
 import AnchorLink from "react-anchor-link-smooth-scroll";
 import useMediaQuery from "../hooks/useMediaQuery";
-const Link =({page, selectedPage, setSelectedPage})=>{
-return (
-    <AnchorLink className={`${selectedPage === lowerCasePage}`}></AnchorLink>
-)
-}
+
+const Link = ({ page, selectedPage, setSelectedPage }) => {
+  const lowerCasePage = page.toLowerCase();
+  return (
+    <AnchorLink
+      className={`${
+        selectedPage === lowerCasePage ? "text-yellow" : ""
+      }  hover:text-yellow transition duration-500`}
+      href={`#${lowerCasePage}`}
+      onClick={() => setSelectedPage(lowerCasePage)}
+    >
+      {page}
+    </AnchorLink>
+  );
+};
 
 export const NavBar = ({ setSelectedPage, selectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
@@ -13,9 +23,35 @@ export const NavBar = ({ setSelectedPage, selectedPage }) => {
   return (
     <nav className="z-40 w-full top-0 fixed py-6">
       <div className=" flex items-center justify-between mx-auto w-5/6">
-        <h4 className="font-playfair text-3xl font-bold">
-          {isAboveSmallScreens ? <div className="flex justify-between gap-16 font-opensans text-sm  font-semibold"></div> : <div></div>}
-        </h4>
+        <h4 className="font-playfair text-3xl font-bold">KUNDUZ</h4>
+        {isAboveSmallScreens ? (
+          <div className="flex justify-between gap-16 font-opensans text-sm  font-semibold">
+            <Link
+              page="Home"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Skills"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Projects"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+            <Link
+              page="Contact"
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          </div>
+        ) : (
+          <button>
+            <img alt="menu"/>
+          </button>
+        )}
       </div>
     </nav>
   );
