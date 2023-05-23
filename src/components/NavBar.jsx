7b2bf51114f076e63616707501b3756bcd200955
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-05-23 14:10:21 +0600

    components - add dotGroup

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -17,11 +17,12 @@ const Link = ({ page, selectedPage, setSelectedPage }) => {
   );
 };
 
-export const NavBar = ({ setSelectedPage, selectedPage }) => {
+export const NavBar = ({isTopOfPage, setSelectedPage, selectedPage }) => {
   const [isMenuToggled, setIsMenuToggled] = useState(false);
   const isAboveSmallScreens = useMediaQuery("(min-width:768px)");
+  const navbarBackground = isTopOfPage ? '' : "bg-red"
   return (
-    <nav className="z-40 w-full top-0 fixed py-6">
+    <nav className={`${navbarBackground} z-40 w-full top-0 fixed py-6`}>
       <div className=" flex items-center justify-between mx-auto w-5/6">
         <h4 className="font-playfair text-3xl font-bold">KUNDUZ</h4>
 
