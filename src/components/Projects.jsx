e20f75cebd907a3111eed72955ee483303a6500f
Kunduz Rahat <rahatova.kunduz@gmail.com>
2023-05-31 16:48:14 +0600

    add hover

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { LineGradient } from "./LineGradient";
+import { FaCode, FaPlay } from "react-icons/fa";
 import { motion } from "framer-motion";
-
+import { projectsData } from "../data";
 const container = {
   hidden: {},
   visible: {
@@ -16,24 +17,24 @@ const projectVariant = {
   visible: { opacity: 1, scale: 1 },
 };
 
-const Project = ({ title }) => {
-  const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
-      bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
-  const projectTitle = title.split(" ").join("-").toLowerCase();
+// const Project = ({ title }) => {
+//   const overlayStyles = `absolute h-full w-full opacity-0 hover:opacity-90 transition duration-500
+//       bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+//   const projectTitle = title.split(" ").join("-").toLowerCase();
 
-  return (
-    <motion.div variants={projectVariant} className="relative">
-      <div className={overlayStyles}>
-        <p className="text-2xl font-playfair">{title}</p>
-        <p className="mt-7">
-          Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
-          porttitor accumsan tincidunt.
-        </p>
-      </div>
-      <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
-    </motion.div>
-  );
-};
+//   return (
+//     <motion.div variants={projectVariant} className="relative">
+//       <div className={overlayStyles}>
+//         <p className="text-2xl font-playfair">{title}</p>
+//         <p className="mt-7">
+//           Curabitur arcu erat, accumsan id imperdiet et, porttitor at sem. Nulla
+//           porttitor accumsan tincidunt.
+//         </p>
+//       </div>
+//       <img src={`../assets/${projectTitle}.jpeg`} alt={projectTitle} />
+//     </motion.div>
+//   );
+// };
 export const Projects = () => {
   return (
     <section id="projects" className="pt-48 pb-48 mt-5">
@@ -57,7 +58,7 @@ export const Projects = () => {
             <LineGradient width="w-2/3" />
           </div>
         </div>
-        <p className="mt-10 mb-10 ">
+        <p className="mt-10 mb-10 text-base">
           My projects make use of a vast variety of latest technology tools. My
           best experience is to create NodeJS Back-end Projects, Front-end React
           Project. Below are some of my projects.
@@ -75,24 +76,75 @@ export const Projects = () => {
         >
           {/* ROW 1 */}
           <div
-            className="flex justify-center text-center items-center p-10 bg-red
+            className="flex justify-center text-center items-center p-10 bg-red sm:mt-10 md:mt-0
                   max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
           >
             BEAUTIFUL USER INTERFACES
           </div>
-          <Project title="Project 1" />
+          {/* <Project title="Project 1" />
           <Project title="Project 2" />
 
           {/* ROW 2 */}
-          <Project title="Project 3" />
+          {/* <Project title="Project 3" />
           <Project title="Project 4" />
           <Project title="Project 5" />
 
           {/* ROW 3 */}
-          <Project title="Project 6" />
-          <Project title="Project 7" />
+          {/* <Project title="Project 6" />
+          <Project title="Project 7" />   */}
+
+          {projectsData.map((project) => (
+            <div className=" max-w-[400px] max-h-[400px] sm:mt-10 md:mt-0">
+           
+              <div className="shadow-lg group container    max-w-sm flex justify-center items-center  mx-auto ">
+                <div>
+                  <div className="w-full ">
+             
+                     <img 
+                    src={project.image}
+                    alt={project.name}
+                    className=" h-48 w-full object-cover object-hidden  group-hover:opacity-25 "
+                  />
+                  </div>
+                  <div className="py-8 px-4 bg-white  group-hover:opacity-25">
+                    <span className="block text-lg text-gray-800 font-bold tracking-wide">
+                      {project.name}
+                    </span>
+                    <span className="block text-black text-sm">
+                      {project.desk}
+                    </span>
+                  </div>
+                </div>
+                <div className="absolute opacity-0  group-hover:opacity-100">
+                  {project.tags.map((tag, id) => (
+                    <p
+                      className="font-medium text-white tracking-wider leading-relaxed"
+                      key={id}
+                    >
+                      {tag}
+                    </p>
+                  ))}
+                  <div className="text-white flex justify-between">
+                    <a
+                      className="cursor-pointer no-underline transition-transform"
+                      href={project.demo}
+                      target="_blank"
+                      rel="noreferrer"
+                    >
+                      <FaPlay aria-label="Demo" />
+                    </a>
+                    <a href={project.code} target="_blank" rel="noreferrer">
+                      <FaCode aria-label="Code" />
+                    </a>
+                  </div>
+              
+                </div>
+              </div>
+            </div>
+          ))}
+
           <div
-            className="flex justify-center text-center items-center p-10 bg-blue
+            className="flex justify-center text-center items-center p-10 bg-blue sm:mt-10 md:mt-0
                   max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold"
           >
             SMOOTH USER EXPERIENCE
---
+++ src/data/index.js
@@ -0,0 +1,42 @@
+import cinema from '../assets/cinema.jpg'
+import post from '../assets/post1.png'
+import country from '../assets/country.jpg'
+import youtube from '../assets/youtube.jpg'
+ export const projectsData = [
+    {
+        id: 1,
+        name: 'Post MERN App',
+        desk: 'it is a full stack application for creating posts with the ability to authenticate the user',
+        tags: ['React', 'TailWindCss', 'Redux', 'Express'],
+        code: 'https://github.com/Kunduz-rahat/mern_blog',
+        demo: 'https://mern-blog-front-beige.vercel.app/',
+        image: post
+    },
+    {
+        id: 2,
+        name: 'YouTube Clone',
+        desk: 'A search engine and YouTube video player, made with React.js.',
+        tags: ['React', 'RapidApi', 'Material Ui'],
+        code: 'https://github.com/Kunduz-rahat/youtube_clone',
+        demo: 'https://youtube-clone-pyumwwp86-kunduz-rahat.vercel.app/',
+        image: youtube
+    },
+    {
+        id: 3,
+        name: 'Country App',
+        desk: 'CountryApp is a source code application for getting information about countries, capitals.',
+        tags: ['React', 'SaSS', 'Redux', 'TypeScript'],
+        code: 'https://github.com/Kunduz-rahat/countries_ts',
+        demo: 'https://countries-kfewl0kfn-kunduz-rahat.vercel.app/',
+        image: country
+    },
+    {
+        id: 4,
+        name: 'Movie Application',
+        desk: 'A simple application for viewing information about movies, trailers, actors',
+        tags: ['React', 'Redux', 'TailwindCss', 'TypeScript'],
+        code: 'https://github.com/Kunduz-rahat/movie-ts',
+        demo: 'https://movie-h8t66otpc-kunduz-rahat.vercel.app/',
+        image: cinema
+    }
+]
